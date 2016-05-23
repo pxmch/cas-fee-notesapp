1e95de3c080297e43f2d47f5f76a2670a859b41b
jisi <isa.steiner@siroop.ch>
2016-05-23 08:57:06 +0200

    new list structure

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,1 +1,10 @@
-console.log('main.js');
+(function () {
+
+  'use strict';
+
+  $('.edit-toggle').on('click', function () {
+    $('.edit-dialog').toggleClass('edit-dialog-open');
+  })
+
+})();
+
--- app/scripts/menu.js
+++
@@ -1,9 +0,0 @@
-/**
- * Created by isa on 16/05/16.
- */
-
-(function () {
-
-  'use strict';
-
-})();
