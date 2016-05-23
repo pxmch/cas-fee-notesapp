08269a7ff56a5182e8923c253ec86195016da49d
jisi <isa.steiner@siroop.ch>
2016-05-23 23:26:39 +0200

    toggle style

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -4,7 +4,11 @@
 
   $('.edit-toggle').on('click', function () {
     $('.edit-dialog').toggleClass('edit-dialog-open');
-  })
+  });
+
+  $('.style-toggle').on('click', function () {
+    $('body').toggleClass('style-change');
+  });
 
 })();
 
